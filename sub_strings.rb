1ac20762bf238b29701a0c7783e1bdf53553da05
Ubuntu <ubuntu@ip-172-31-61-48.ec2.internal>
2024-07-30 22:37:56 +0000

    completed substring project

--- sub_strings.rb
+++ sub_strings.rb
@@ -5,7 +5,7 @@ def sub_strings(str, dict)
     dict.map do |entry|
         # split str on substring and get the length minus one for number of occurences
         # uncomment the next line to see how this works
-        puts "Substring: #{entry}, result: #{str.split(entry)}"
+        # puts "Substring: #{entry}, result: #{str.split(entry)}"
         count_length = str.split(entry).length - 1
         if (count_length > 0)
             hash[entry] = count_length
